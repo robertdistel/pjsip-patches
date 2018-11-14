0874317ec5c93bf6e375b565355cda6051d46b18
Rob Distel <robert.distel@softelsystems.com.au>
2018-11-15 08:42:13 +1100

    Changes to site local config for xbar rather than conference support and other efficiency imporvements

--- pjlib/include/pj/config_site.h
+++ pjlib/include/pj/config_site.h
@@ -5,5 +5,6 @@
 #define PJSUA_MAX_CALLS (2045)
 #define PJSIP_MAX_DIALOG_COUNT (2048-1)
 #define PJ_OVERRIDE__FD_SETSIZE (4096) //see pj/compat/socket.h patch
-
+#define PJMEDIA_CONF_USE_SWITCH_BOARD (1) 
+#define PJSIP_SAFE_MODULE (0)  //we do not want/need dynamic loading of modules
 #endif
